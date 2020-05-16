77221e3f85019f9def341b53dc68c44da1d5b56d
Miika Somero <miika.somero@gmail.com>
2020-05-16 14:55:38 +0300

    fixed foreign key bug for sqlite

--- application/__init__.py
+++ application/__init__.py
@@ -9,17 +9,17 @@ else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///skills.db"
     app.config["SQLALCHEMY_ECHO"] = True
 
+    from sqlalchemy.engine import Engine
+    from sqlalchemy import event
+
+    @event.listens_for(Engine, "connect")
+    def set_sqlite_pragma(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
+
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
-
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
-
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
 
 from application import models
 from application import views
