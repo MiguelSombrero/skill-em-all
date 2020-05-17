817350083a4d9eee7b45ca7fad90a911d5f98c7c
Miika Somero <miika.somero@gmail.com>
2020-05-17 13:55:17 +0300

    more styling

--- application/skills/forms.py
+++ application/skills/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators
 
 class SkillForm(FlaskForm):
-    name = StringField("Name",
+    name = StringField("Name of the skill",
         [validators.Length(min=2, max=50, message="Skill name must be between 2-50 characters")]
     )
 
