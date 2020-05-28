ca0d0e70cd3347682965be6fcdffacab2e777ab3
Miika Somero <miika.somero@gmail.com>
2020-05-28 21:13:20 +0300

    added styling for project page

--- application/skills/models.py
+++ application/skills/models.py
@@ -15,7 +15,7 @@ class Skill(Base, UserResource):
     @staticmethod
     def find_skills_by_project(project_id):
         statement = text(
-            "SELECT Skill.name, SUM(Experience.experience)"
+            "SELECT Skill.name, SUM(Experience.experience), MAX(Experience.experience)"
             " FROM Account_project"
             " LEFT JOIN Account ON Account.id = Account_project.account_id"
             " LEFT JOIN Skill ON Skill.owner_id = Account.id"
@@ -30,7 +30,8 @@ class Skill(Base, UserResource):
         for row in res:
             response.append({
                 "name": row[0],
-                "experience": row[1]
+                "experience": row[1],
+                "max": row[2]
             })
 
         return response
