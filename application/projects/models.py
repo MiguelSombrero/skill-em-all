bbe9539118ce112a40f4b76c476018b78ea9cc23
Miika Somero <miika.somero@gmail.com>
2020-06-14 16:56:42 +0300

    added my projects view info about staff and changed related query accordingly

--- application/projects/models.py
+++ application/projects/models.py
@@ -19,20 +19,54 @@ class Project(Base, UserResource):
     @staticmethod
     def find_projects_by_owner(owner_id):
         statement = text(
-            "SELECT Project.id, Project.name, Project.start_date, Project.end_date"
+            "SELECT Project.*, Account.name"
             " FROM Project"
             " LEFT JOIN Account_project ON Project.id = Account_project.project_id"
             " LEFT JOIN Account ON Account.id = Account_project.account_id"
             " WHERE Project.owner_id = :owner_id"
-            " AND Project.active = 1"
-            " GROUP BY Project.id"
+            " GROUP BY Project.id, Account.name"
         ).params(owner_id=owner_id)
 
         res = db.engine.execute(statement)
-
         response = []
+
         for row in res:
-            response.append(row)
+            id = row[0]
+            name = row[2]
+            start_date = row[3]
+            end_date = row[4]
+            active = row[5]
+            staff_name = row[7]
+
+            isInList = False
+
+            for project in response:
+                if project["id"] == id:
+                    isInList = True
+
+            if isInList:
+                last = len(response) - 1
+
+                response[last]["staff"].append({
+                    "name": staff_name
+                })
+
+            else:
+                new = {
+                    "id": id,
+                    "name": name,
+                    "start_date": start_date,
+                    "end_date": end_date,
+                    "active": active,
+                    "staff": []
+                }
+
+                if staff_name:
+                    new["staff"].append({
+                        "name": staff_name
+                    })
+
+                response.append(new)
 
         return response
 
